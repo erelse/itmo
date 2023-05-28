6cd463ca231fc7341efa73903274c6ae9a9c5c19
erelse <erelse@yandex.ru>
2023-05-28 19:23:03 +0300

    add Load.java

--- src/ru/itmo/lessons/kurs02/Load.java
+++ src/ru/itmo/lessons/kurs02/Load.java
@@ -15,54 +15,15 @@ public class Load extends Menu implements Serializable {
     public void execute() {
 
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("savedGame"))) {
-            Game gameObject = (Game) ois.readObject();
+            Game game = (Game) ois.readObject();
 
-           // System.out.printf(gameObject.getText());
-         /*   System.out.printf(game.getFirst());
-            System.out.printf(game.getSecond());*/
-
-          // game = new Game(gameObject.getFirst(), gameObject.getFirst(),
-                    gameObject.getText());
-
-
+            game.getText();
 
         } catch (Exception ex) {
 
-
             System.out.println(ex.getMessage());
         }
-        new Start (game).execute();
+        new Start(game).execute();
     }
 
 }
-
-
-
-
-
-   /* }
-        try (ObjectInputStream objectInputStream = new ObjectInputStream(
-                new FileInputStream("savedGame"))) {
-            ObjectOfTheGame gameObject= ((ObjectOfTheGame) objectInputStream.readObject());
-
-
-outputStream.writeObject(gameObject);
-
-        } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        new Start(gameObject).execute();
-*/
-
-/* System.out.println(gameObject.getText());*//*
-
-    }
-}
-
-   */
-/* ObjectInputStream objectInputStream = new ObjectInputStream(
-            new FileInputStream("person.out"));
-    Person igorRestored = (Person) objectInputStream.readObject();
-    Person renatRestored = (Person) objectInputStream.readObject();
-        objectInputStream.close();
-*/
